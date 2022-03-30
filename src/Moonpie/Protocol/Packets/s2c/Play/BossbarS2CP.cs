95faa90a87c01e1a005ef0e913076cffd8d32a2f
Stay <quiquelluc@gmail.com>
2022-03-30 02:16:35 +0200

    Bossbar Packet

--- src/Moonpie/Protocol/Packets/s2c/Play/BossbarS2CP.cs
+++ src/Moonpie/Protocol/Packets/s2c/Play/BossbarS2CP.cs
@@ -26,19 +26,119 @@
 
 using Moonpie.Protocol.Network;
 using Moonpie.Protocol.Protocol;
+using Moonpie.Utils.Protocol;
 
 namespace Moonpie.Protocol.Packets.s2c.Play;
 
 [PacketType(PacketTypes.S2C.PLAY_BOSS_BAR)]
 public class BossbarS2CP : IS2CPacket
 {
+    public enum BossbarAction
+    {
+        Add,
+        Remove,
+        UpdateHealth,
+        UpdateTitle,
+        UpdateStyle,
+        UpdateFlags
+    }
+
+    public enum BossbarColor
+    {
+        Pink,
+        Blue,
+        Red,
+        Green,
+        Yellow,
+        Purple,
+        White
+    }
+
+    public enum BossbarDivision
+    {
+        NoDivision,
+        Notches6,
+        Notches10,
+        Notches12,
+        Notches20
+    }
+    
+    public JavaUUID Uuid { get; set; }
+    public BossbarAction Action { get; set; }
+    public ChatComponent? Title { get; set; }
+    public float? Health { get; set; }
+    public BossbarColor? Color { get; set; }
+    public BossbarDivision? Division { get; set; }
+    public byte? Flags { get; set; }
+    
     public void Read(InByteBuffer buffer)
     {
-        throw new NotImplementedException();
+        Uuid = buffer.ReadUUID();
+        Action = (BossbarAction)buffer.ReadVarInt();
+        
+        switch (Action)
+        {
+            case BossbarAction.Add:
+                Title = buffer.ReadChatComponent();
+                Health = buffer.ReadFloat();
+                Color = (BossbarColor)buffer.ReadVarInt();
+                Division = (BossbarDivision)buffer.ReadVarInt();
+                Flags = buffer.ReadByte();
+                break;
+            case BossbarAction.Remove:
+                // nothing
+                break;
+            case BossbarAction.UpdateHealth:
+                Health = buffer.ReadFloat();
+                break;
+            case BossbarAction.UpdateTitle:
+                Title = buffer.ReadChatComponent();
+                break;
+            case BossbarAction.UpdateStyle:
+                Color = (BossbarColor)buffer.ReadVarInt();
+                Division = (BossbarDivision)buffer.ReadVarInt();
+                break;
+            case BossbarAction.UpdateFlags:
+                Flags = buffer.ReadByte();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
     }
 
     public void Write(OutByteBuffer buffer)
     {
-        throw new NotImplementedException();
+        buffer.WriteUUID(Uuid);
+        buffer.WriteVarInt((int)Action);
+        
+        switch (Action)
+        {
+            case BossbarAction.Add:
+                buffer.WriteChatComponent(Title ?? ChatComponent.Empty);
+                buffer.WriteFloat(Health ?? 0);
+                buffer.WriteVarInt((int)(Color ?? 0));
+                buffer.WriteVarInt((int)(Division ?? 0));
+                buffer.WriteByte(Flags ?? 0);
+                break;
+            case BossbarAction.Remove:
+                // nothing
+                break;
+            case BossbarAction.UpdateHealth:
+                buffer.WriteFloat(Health ?? 0);
+                break;
+            case BossbarAction.UpdateTitle:
+                buffer.WriteChatComponent(Title ?? ChatComponent.Empty);
+                break;
+            case BossbarAction.UpdateStyle:
+                buffer.WriteVarInt((int)(Color ?? 0));
+                buffer.WriteVarInt((int)(Division ?? 0));
+                break;
+            case BossbarAction.UpdateFlags:
+                buffer.WriteByte(Flags ?? 0);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
