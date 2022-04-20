a95d507cb7c6476934523af63685554937d01790
RemineHD <reminegames@gmail.com>
2022-04-20 22:44:36 +0200

    Implementation of Internal Plugin
    Added: moonpie Command (For Proxy Info and Spam)
    Added: whereami Command (For debugging)
    *Variables are temporal, not functional.

---
+++ src/Moonpie/InternalPlugin/InternalCommands.cs
@@ -0,0 +1,25 @@
+﻿using Moonpie.Plugins;
+using Moonpie.Plugins.Attributes;
+using Moonpie.Protocol.Protocol;
+
+namespace Moonpie.InternalPlugin;
+
+public class InternalCommands : BaseCommandModule
+{
+    
+    [Command("moonpie")]
+    public async Task Moonpie(CommandContext ctx)
+    {
+        await ctx.Player.SendMessageAsync(
+            ChatComponent.Parse("\n &9Moonpie Proxy  \n&8│ &fThis network is using Moonpie. \n&8│ &fIn its version: &3" + ctx.Player.Version + "\n&8│ &fMore info: &dhttps://github.com/Stay1444/Moonpie\n", '&'));
+    }
+    
+    
+    [Command("whereami")]
+    public async Task Whereami(CommandContext ctx)
+    {
+        await ctx.Player.SendMessageAsync(
+            ChatComponent.Parse("&7You are on proxy: &3proxy01\n&7On the server: " + ctx.Player.Transport.ServerTransport!.Connection.RemoteEndPoint + "\n&7With coords: &9X &7-> &30 &9Y &7-> &30 &9Z &7-> &30", '&'));
+    }
+    
+}
---
+++ src/Moonpie/InternalPlugin/InternalPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Moonpie.Plugins;
+using Serilog;
+
+namespace Moonpie.InternalPlugin;
+
+public class InternalPlugin : MoonpiePlugin
+{
+    public override Task OnLoad()
+    {
+        Log.Information("InternalPlugin loaded");
+        RegisterCommands(new InternalCommands());
+        return Task.CompletedTask;
+    }
+
+    public override Task OnUnload()
+    {
+        return Task.CompletedTask;
+    }
+}
