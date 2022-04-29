7ebb862e993d06baa21fae09cc5fa7d3ef35c274
Stay1444 <quiquelluc@gmail.com>
2022-04-30 01:28:43 +0200

    Difficulty Packet

---
+++ src/Moonpie/Protocol/Packets/s2c/Play/ServerDifficultyS2CP.cs
@@ -0,0 +1,34 @@
+using Moonpie.Protocol.Network;
+using Moonpie.Protocol.Protocol;
+using Moonpie.Utils.Protocol;
+
+namespace Moonpie.Protocol.Packets.s2c.Play;
+
+[PacketType(PacketTypes.S2C.PLAY_SERVER_DIFFICULTY)]
+public class ServerDifficultyS2CP : IS2CPacket
+{
+    public enum Difficulties
+    {
+        Peaceful = 0,
+        Easy = 1,
+        Normal = 2,
+        Hard = 3
+    }
+    
+    public Difficulties Difficulty { get; set; }
+    public bool? Locked { get; set; }
+    public void Read(InByteBuffer buffer)
+    {
+        Difficulty = (Difficulties)buffer.ReadByte();
+        Locked = buffer.ReadOptional(buffer.ReadBoolean);
+    }
+
+    public void Write(OutByteBuffer buffer)
+    {
+        buffer.WriteByte((byte)Difficulty);
+        if (Locked is not null)
+        {
+            buffer.WriteBoolean(Locked.Value);
+        }
+    }
+}
