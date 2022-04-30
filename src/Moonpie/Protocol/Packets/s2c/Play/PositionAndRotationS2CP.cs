49017bb60ba88428872c19573573091f56355089
Stay1444 <quiquelluc@gmail.com>
2022-04-30 20:05:53 +0200

    Position And Rotation Packet

---
+++ src/Moonpie/Protocol/Packets/s2c/Play/PositionAndRotationS2CP.cs
@@ -0,0 +1,37 @@
+using Moonpie.Protocol.Network;
+using Moonpie.Protocol.Protocol;
+using Moonpie.Utils.Math;
+using Moonpie.Utils.Protocol;
+
+namespace Moonpie.Protocol.Packets.s2c.Play;
+
+[PacketType(PacketTypes.S2C.PLAY_POSITION_AND_ROTATION)]
+public class PositionAndRotationS2CP : IS2CPacket
+{
+    public Vector3d Position { get; set; }
+    public float Yaw { get; set; }
+    public float Pitch { get; set; }
+    public byte Flags { get; set; }
+    public int TeleportId { get; set; }
+    public bool Dismount { get; set; }
+    
+    public void Read(InByteBuffer buffer)
+    {
+        Position = buffer.ReadVector3d();
+        Yaw = buffer.ReadFloat();
+        Pitch = buffer.ReadFloat();
+        Flags = buffer.ReadByte();
+        TeleportId = buffer.ReadVarInt();
+        Dismount = buffer.ReadBoolean();
+    }
+
+    public void Write(OutByteBuffer buffer)
+    {
+        buffer.WriteVector3d(Position);
+        buffer.WriteFloat(Yaw);
+        buffer.WriteFloat(Pitch);
+        buffer.WriteByte(Flags);
+        buffer.WriteVarInt(TeleportId);
+        buffer.WriteBoolean(Dismount);
+    }
+}
