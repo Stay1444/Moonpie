83b3be1aba8a26d3d7f4aca8b13a9f30feabef6f
Stay1444 <quiquelluc@gmail.com>
2022-04-30 02:39:15 +0200

    Refactor

--- src/Moonpie/Plugins/Internal/InternalCommands.cs
+++ src/Moonpie/Plugins/Internal/InternalCommands.cs
@@ -1,5 +1,4 @@
-﻿using Moonpie.Plugins;
-using Moonpie.Plugins.Attributes;
+﻿using Moonpie.Plugins.Attributes;
 using Moonpie.Protocol.Protocol;
 
 namespace Moonpie.Plugins.Internal;
@@ -10,16 +9,11 @@ public class InternalCommands : BaseCommandModule
     [Command("moonpie")]
     public async Task Moonpie(CommandContext ctx)
     {
-        await ctx.Player.SendMessageAsync(
-            ChatComponent.Parse("\n &9Moonpie Proxy  \n&8│ &fThis network is using Moonpie. \n&8│ &fIn its version: &3" + ctx.Player.Version + "\n&8│ &fMore info: &dhttps://github.com/Stay1444/Moonpie\n", '&'));
+        await ctx.Player.SendMessageLinesAsync(
+            ChatComponent.From(ChatColor.Blue, "Moonpie"),
+            ChatComponent.From(ChatColor.DarkGray, "| ", ChatColor.Gray, "This network is using Moonpie."),
+            ChatComponent.From(ChatColor.DarkGray, "| ", ChatColor.Gray, "Version: ", ctx.Player.Version),
+            ChatComponent.From(ChatColor.DarkGray, "| ", ChatColor.Gray, "More info: ", ChatColor.Aqua, "https://github.com/Stay1444/Moonpie")
+        );
     }
-    
-    
-    [Command("whereami")]
-    public async Task Whereami(CommandContext ctx)
-    {
-        await ctx.Player.SendMessageAsync(
-            ChatComponent.Parse("&7You are on proxy: &3proxy01\n&7On the server: " + ctx.Player.Transport.ServerTransport!.Connection.RemoteEndPoint + "\n&7With coords: &9X &7-> &30 &9Y &7-> &30 &9Z &7-> &30", '&'));
-    }
-    
 }
