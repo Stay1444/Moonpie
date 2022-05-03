d332624ac6b34a2b90b47394fa5ab757fe3adb99
Stay1444 <quiquelluc@gmail.com>
2022-05-03 20:14:26 +0200

    Refactor & Cleanup

--- src/Moonpie/Protocol/Protocol/PlayerProperties.cs
+++ src/Moonpie/Protocol/Protocol/PlayerProperties.cs
@@ -24,11 +24,7 @@
 // SOFTWARE.
 #endregion
 
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Moonpie.Protocol.Protocol;
 
