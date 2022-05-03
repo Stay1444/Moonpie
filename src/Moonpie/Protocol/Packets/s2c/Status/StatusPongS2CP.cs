cd8174cf638e5c9b6f730736c620b78bffc96414
Stay1444 <quiquelluc@gmail.com>
2022-05-03 20:24:38 +0200

    Refactor & Cleanup

--- src/Moonpie/Protocol/Packets/s2c/Status/StatusPongS2CP.cs
+++ src/Moonpie/Protocol/Packets/s2c/Status/StatusPongS2CP.cs
@@ -33,12 +33,7 @@ namespace Moonpie.Protocol.Packets.s2c.Status;
 public class StatusPongS2CP : IS2CPacket
 {
     public long PingId { get; set; }
-
-    public void Log()
-    {
-        Console.WriteLine($"StatusPongS2CP: PingId: {PingId}");
-    }
-
+    
     public void Read(InByteBuffer buffer)
     {
         PingId = buffer.ReadLong();
